c7b12014cd2f482c77eee9247f811a318fe82b13
Dhiraj Subedi <dhirajsubedi924@gmail.com>
2022-06-13 15:11:57 +0545

    hrere brocast stream

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  StreamController myStreamController=StreamController();
+ late Stream myStream=myStreamController.stream.asBroadcastStream();
+void initState(){
+myStream.listen((event) {
+  print(event);
+});
+myStream.listen((event) {
+  print(event*2);
+});
+  super.initState();
+}
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+       
+        title: Text(widget.title),
+      ),
+      body: Center(
+       
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: ()async{
+          myStreamController.sink.add(4);
+          // print(myStreamController.stream.toString());
+        },
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
